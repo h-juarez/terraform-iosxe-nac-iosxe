cdd5239fc6bdbc8b607a1d2a529a588118ffa86a
Hector Juarez <hjuarez@cisco.com>
2025-07-15 13:56:08 -0600

    add default values to mdf_subscription

--- iosxe_mdt_subscription.tf
+++ iosxe_mdt_subscription.tf
@@ -13,15 +13,15 @@ resource "iosxe_mdt_subscription" "mdt_subscriptions" {
 
   device                  = each.value.device
   subscription_id         = each.value.subscription.id
-  stream                  = try(each.value.subscription.stream, null)
-  encoding                = try(each.value.subscription.encoding, null)
-  source_vrf              = try(each.value.subscription.source_vrf, null)
-  source_address          = try(each.value.subscription.source_address, null)
-  update_policy_on_change = try(each.value.subscription.update_policy_on_change, null)
-  filter_xpath            = try(each.value.subscription.filter_xpath, null)
+  stream                  = try(each.value.subscription.stream, local.defaults.iosxe.configuration.mdt_subscriptions.stream, null)
+  encoding                = try(each.value.subscription.encoding, local.defaults.iosxe.configuration.mdt_subscriptions.encoding, null)
+  source_vrf              = try(each.value.subscription.source_vrf, local.defaults.iosxe.configuration.mdt_subscriptions.source_vrf, null)
+  source_address          = try(each.value.subscription.source_address, local.defaults.iosxe.configuration.mdt_subscriptions.source_address, null)
+  update_policy_on_change = try(each.value.subscription.update_policy_on_change, local.defaults.iosxe.configuration.mdt_subscriptions.update_policy_on_change, null)
+  filter_xpath            = try(each.value.subscription.filter_xpath, local.defaults.iosxe.configuration.mdt_subscriptions.filter_xpath, null)
 
   receivers = [
-    for r in try(each.value.subscription.receivers, []) : {
+    for r in try(each.value.subscription.receivers, local.defaults.iosxe.configuration.mdt_subscriptions.receivers, []) : {
       address  = r.address
       port     = r.port
       protocol = r.protocol
